4fc3dd4fa7f0826d8ca28fbe9fb5ee085e5a813c
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2020-12-15 11:57:06 -0300

    Respondendo request com base nas urls

--- noticias.js
+++ noticias.js
@@ -3,6 +3,15 @@
 //console.log("Criando site de noticias");
 var http = require('http');
 var server = http.createServer(function (req, res) {
-    res.end("<html><body>Portal de noticias</body></html>");
-});
-server.listen(3000);
+
+    var categoria = req.url;
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Tech</body></html>");
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Moda</body></html>");
+    } else if (categoria == '/beleza') {
+        res.end("<html><body>Beleza</body></html>");
+    } else {
+        res.end("<html><body>Portal de noticias</body></html>");
+    }
+}).listen(3000);
