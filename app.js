f570ddef8f59e9187b9cd424eff7e629f3165930
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2020-12-20 01:21:34 -0300

    Refactoring with EJS

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     //res.send Trabalhando com express
-    //res.render Ejs junto com express permigindo código .js
-    res.render("secao/home");
+    //res.render Ejs junto com express pertindoindo código .js
+    res.render("home/index");
 });
-app.get('/noticia', function(req,res){
-    res.render('secao/noticia')
+app.get('/noticia', function (req, res) {
+    res.render("noticias/noticia");
     //res.send("<html><body>Moda</body></html>"); 
 });
-app.get('/tech', function(req,res){
+app.get('/admin', function (req, res) {
+    res.render("admin/form_add_noticia");
+});
+app.get('/tech', function (req, res) {
     res.render("secao/tech");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Servidor rodando com express")
 });
 
