31f714d88bec98da5d32f9439a2c3e66daf8d1bc
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2020-12-20 22:50:55 -0300

    routes done and tests with images

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 var app = require('./config/server'); // ... = require('./') Parte do mesmo nivel do diretório
 
 var rotaNoticia = require('./app/routes/noticia');
-rotaNoticia (app);
+rotaNoticia(app);
 
-var rotaHome = require ('./app/routes/home')(app);
-var rotaForm = require ('./app/routes/form_add_noticia')(app);
-var rotaAdmin = require ('./app/routes/admin')(app);
-var rotaNoticia = require ('./app/routes/noticia')(app);
-var rotaTech = require ('./app/routes/tech')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaForm = require('./app/routes/form_add_noticia')(app);
+var rotaAdmin = require('./app/routes/admin')(app);
+var rotaNoticia = require('./app/routes/noticia')(app);
+var rotaTech = require('./app/routes/tech')(app);
 
 app.listen(3000, function () {
-    console.log("Servidor On")
+    console.log("Servidor ON")
 });
 
