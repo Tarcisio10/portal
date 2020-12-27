7f41c1a46e9f72dde674a88fe4ad156fe82d9af5
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2020-12-27 11:30:19 -0300

    Refactoring com Consign, autoload das rotas

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict'
 var app = require('./config/server'); // ... = require('./') Parte do mesmo nivel do diretório
 
-var rotaHome = require('./app/routes/home')(app);
-var rotaForm = require('./app/routes/form_add_noticia')(app);
-var rotaAdmin = require('./app/routes/admin')(app);
-var rotaNoticias = require('./app/routes/noticias')(app);
-var rotaTech = require('./app/routes/tech')(app);
+//var rotaHome = require('./app/routes/home')(app);
+//var rotaForm = require('./app/routes/form_add_noticia')(app);
+//var rotaAdmin = require('./app/routes/admin')(app);
+//var rotaNoticias = require('./app/routes/noticias')(app);
+//var rotaTech = require('./app/routes/tech')(app);
 
 app.listen(3002, function () {
     console.log("Servidor ON")
